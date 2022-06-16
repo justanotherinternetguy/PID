58d7850952cc75f32ae10e0d968550e0ead78121
justanotherinternetguy <96833060+justanotherinternetguy@users.noreply.github.com>
2022-06-16 01:01:17 +0000

    Update main.py

--- main.py
+++ main.py
@@ -12,14 +12,15 @@ import math
 
 SP = 100 # setpoint (target)
 PV = 0 # process value (what is being controlled)
-time = 50
+time = 20
 
-Kp = 0.7
-Ki = 0.8
-Kd = 0
+Kp = 0.4
+Ki = 0.4
+Kd = 0.4
 
 
 summation = 0
+lastET = SP-PV
 
 xpoints = list(np.arange(1, time+1)) # time
 ypoints = np.array([])
@@ -29,24 +30,33 @@ def proportional(SP, Kp, PV):
     return (Kp * (SP - PV))
 
 def integral(SP, Ki, PV, summation):
+    return Ki * summation
+
+def summate(SP, PV, summation):
     et = (SP - PV) # error
     dt = (1/time) # cycle time???
     summation += et * dt
-    return Ki * summation
+    return summation
 
-def derivative(SP, Kd, PV):
-    return 0
+
+def derivative(SP, Kd, PV, lastEt):
+    et = (SP - PV) # current error
+    etDELTA = et - lastEt
+    dt = (1/time)
+    return Kd * (etDELTA / dt)
 
 
 def output(P, I, D):
     return P+I+D+SP
 
+
 for i in range(time):
-    PV = output(proportional(SP, Kp, PV), integral(SP, Ki, PV, summation), derivative(SP, Kd, PV)) 
-    summation = integral(SP, Ki, PV, summation)
-    
+    lastET = (SP - PV)
     ypoints = np.append(ypoints, PV)
     test.append(PV)
+    summation = summate(SP, PV, summation)
+    PV = output(proportional(SP, Kp, PV), integral(SP, Ki, PV, summation), derivative(SP, Kd, PV, lastET)) 
+
     print(summation)
 
 
