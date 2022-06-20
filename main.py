3d643ee0b653f0b0ed347090fbbab72f8010e4bb
justanotherinternetguy <96833060+justanotherinternetguy@users.noreply.github.com>
2022-06-20 18:13:28 -0400

    e
    e

--- main.py
+++ main.py
@@ -4,11 +4,10 @@
 
 import matplotlib.pyplot as plt
 import numpy as np
-import math
 
 
-SP = 100 # setpoint (target)
-PV = 0 # process value (what is being controlled)
+SP = 100  # setpoint (target)
+PV = 0  # process value (what is being controlled)
 time = 20
 
 Kp = 0.4
@@ -17,29 +16,32 @@ Kd = 0.4
 
 
 summation = 0
-lastET = SP-PV
+lastET = SP - PV
 
-xpoints = list(np.arange(1, time+1)) # time
+xpoints = list(np.arange(1, time+1))  # time
 ypoints = np.array([])
 test = []
+
 
 def proportional(SP, Kp, PV):
     return (Kp * (SP - PV))
 
+
 def integral(SP, Ki, PV, summation):
     return Ki * summation
 
-def summate(SP, PV, summation):
-    et = (SP - PV) # error
-    dt = (1/time) # cycle time???
-    summation += et * dt
-    return summation
+# def summate(SP, PV, summation):
+#     et = (SP - PV) # error
+#     dt = (1/time) # cycle time???
+#     summation += et * dt
+#     return summation
 
 
 def derivative(SP, Kd, PV, lastEt):
-    et = (SP - PV) # current error
+    et = (SP - PV)  # current error
     etDELTA = et - lastEt
     dt = (1/time)
+    # print(et, etDELTA, dt)
     return Kd * (etDELTA / dt)
 
 
@@ -51,10 +53,12 @@ for i in range(time):
     lastET = (SP - PV)
     ypoints = np.append(ypoints, PV)
     test.append(PV)
-    summation = summate(SP, PV, summation)
-    PV = output(proportional(SP, Kp, PV), integral(SP, Ki, PV, summation), derivative(SP, Kd, PV, lastET)) 
-
-    print(summation)
+    # summation = summate(SP, PV, summation)
+    PV = output(
+        proportional(SP, Kp, PV),
+        integral(SP, Ki, PV, summation),
+        derivative(SP, Kd, PV, lastET)
+    )
 
 
 print("-------------------------")
